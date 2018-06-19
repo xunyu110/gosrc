3c32f09b1941809ce53b55c43d480aaf6f895d97
xunyu110 <liuhuarongynu@gmail.com>
2018-06-19 14:33:58 +0800

    second

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	)
+
+func getname() (firstname,lastname,nickname string){
+	return "mary","chan","chibi maruko"
+}
+func main() {
+data,_ :=os.Hostname()
+fmt.Println(data)
+	}
+
+	
+
