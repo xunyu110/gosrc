22d35671dce82df22f4d997bf219448847bc3374
liuhuarong <38678018+xunyu110@users.noreply.github.com>
2018-06-24 11:12:49 +0800

    Create range.go

---
+++ range.go
@@ -0,0 +1,32 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	nums :=[]int{2,3,4}
+	sum := 0
+	for _,num :=range nums{
+		sum +=num
+		fmt.Println(num)
+	}
+	
+	fmt.Println("sum:",sum)
+	
+	
+	for i,num :=range nums{
+		if num ==3{
+			fmt.Println("index",i)
+		}
+	}
+	
+	kvs := map[string]string{"a":"apple","b":"banana"}
+	for k,v :=range kvs {
+		fmt.Printf("%s-->%s \n",k,v)
+	}
+		
+	for k:=range kvs {
+	fmt.Println("key:",k)
+	}
+}
